0319f13740c373498f77f7fc62abc5d2148181a8
Marcelo Santos <riquecelox462m@gmail.com>
2022-02-07 16:46:22 -0300

    adição tela modal
    adicionado tela modal e configurações

--- js/script.js
+++ js/script.js
@@ -1,8 +1,19 @@
+/* Variáveis para a iteração do botão lateral do banner*/
 var tituloJogo = document.querySelector('#titulo');
 var numero = document.querySelector('#numero');
 var btnAnterior = document.querySelector('#btn-anterior');
 var btnProximo = document.querySelector('#btn-proximo');
 
+/*Variáveis para a iteração do botão comprar */
+var btnComprar=document.querySelectorAll('.btn-comprar');
+
+/*Variáveis para iteração do botão modal */
+var fecharModal = document.querySelector('.fechar-modal');
+
+
+
+
+/*Logica dos botoes laterais do slide banner */
 var alternador = true;
 
 btnAnterior.addEventListener('click', function(e){
@@ -29,4 +40,30 @@ btnProximo.addEventListener('click', function(e){
         tituloJogo.textContent='MORTAL KOMBAT'
         alternador=true;
     }  
+})
+
+/**Lógica dos botões de compra / ativa modal */
+btnComprar.forEach(function(btn){
+    btn.addEventListener('click', function(){
+        
+        /*Ativação do modal */
+        let modal=document.querySelector('.modal')
+        modal.classList.remove('desativado')
+        modal.classList.add('ativado')
+
+        /*Alterando o número da sacola no menu*/
+        let contador = document.querySelector('.contador')   
+        contador.textContent = '1'
+
+        /*Alteração do botão apos o click */
+        btn.textContent='Comprado!'
+        btn.style.backgroundColor = '#084154'
+    })
+})
+
+/**Lógica do botão modal / fechar modal */
+fecharModal.addEventListener('click', function(){
+    let modal=document.querySelector('.modal')
+        modal.classList.remove('ativado')
+        modal.classList.add('desativado')      
 })
